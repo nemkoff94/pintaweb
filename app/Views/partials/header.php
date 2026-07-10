37266333193fd438634df54448832b686ef4c096
Vladimir Nemkoff <nemkoff94@gmail.com>
2026-07-10 21:56:09 +0300

    header

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -16,6 +16,11 @@ declare(strict_types=1);
         <nav class="nav-right">
             <a href="#events">События</a>
             <a href="#contacts">Контакты</a>
+
+            <div class="contact">
+                <a href="tel:+79533392119" class="phone">+7 953 339 2119</a>
+                <a href="https://vk.com/pinta_mal" class="vk" target="_blank" rel="noopener noreferrer">VK</a>
+            </div>
         </nav>
     </div>
 </header>
